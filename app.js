a7653200ce5842626d5f20af9d27d19317751b79
Arijit Bhowmik <122301763+ARIJIT0483@users.noreply.github.com>
2024-07-14 20:36:33 +0530

    Add Project files

--- app.js
+++ app.js
@@ -119,6 +119,7 @@ app.use((req,res,next)=>{
 // })
 
 app.use("/listings", listingRouter)
+app.use("/", listingRouter)
 app.use("/listings/:id/reviews", reviewRouter)
 app.use("/", userRouter)
 // app.use("/", googleAuth)
